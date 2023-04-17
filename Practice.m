05d5731bdb99b68d26325919409b9ba287f2e78e
AtomicDev <atomicdev09@gmail.com>
2023-04-17 07:50:07 +0545

    Add files via upload

---
+++ Practice.m
@@ -0,0 +1,31 @@
+## - LAB 1
+## ----------- Theory ------------
+
+
+## Matrix - LAB 2
+M1 = [1 4 7; 2 5 6 ; 3 6 9];
+M2 = [-1 4 -6; 2 0 8 ; 3 5 9];
+M1+M2;
+M1-M2;
+M1*M2;
+M1/M2;
+inv(M2);
+det(M1);
+rank(M2);
+
+
+## Complex Numbers - LAB 3
+Z = 5 + 12i;
+W = 3 + 4i;
+
+A=Z+W;
+S=Z-W;
+M=Z*W;
+D=Z/W;
+
+RealPart = real(Z);
+ImaginaryPart = imag(Z);
+Absolute = abs(Z);
+Angle = angle(Z);
+Conjugate = conj(Z);
+
